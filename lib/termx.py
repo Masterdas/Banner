d61319ad54d9b89cb84c3695310caf41674e5527
Mahadeb R..das <123380450+Masterdas@users.noreply.github.com>
2025-05-17 19:17:38 +0530

    Create termx.py

---
+++ lib/termx.py
@@ -0,0 +1,19 @@
+import os
+import time
+import sys
+# Set color
+G = '\033[1;32m' # Green
+
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.002)
+delay_print
+delay_print (""+G+"WW      WW EEEEEEE LL       CCCCC   OOOOO  MM    MM EEEEEEE""\n")
+delay_print (""+G+"WW      WW EE      LL      CC    C OO   OO MMM  MMM EE      ""\n")
+delay_print (""+G+"WW   W  WW EEEEE   LL      CC      OO   OO MM MM MM EEEEE   ""\n")
+delay_print (""+G+" WW WWW WW EE      LL      CC    C OO   OO MM    MM EE      ""\n")
+delay_print (""+G+"  WW   WW  EEEEEEE LLLLLLL  CCCCC   OOOO0  MM    MM EEEEEEE ""\n")
+delay_print (""+G+"                                                            ""\n")
+delay_print (""+G+" <═══════════════Created By 'Mahadeb Ruidas'════════════════>""\n")
